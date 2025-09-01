3d66c0bd44e552bccba44d67a45fd9cd3c25199e
thanhnx12 <thanhnx1209@gmail.com>
2025-09-01 09:12:55 +0000

    better config N_MLP_STREAMS

--- getp-csrc/config.hpp
+++ getp-csrc/config.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-#define BATCH_SIZE 960LL
+#define BATCH_SIZE 32LL
 #define THREADS_PER_BLOCK 256
 #define WARP_SIZE 64
 
 #define MAX_SEQ_LEN 1024
-#define N_MLP_STREAMS 4 // tune me (4–8 is usually good)
+#define N_MLP_STREAMS 8 // tune me (4–8 is usually good)
 
 #define MAX_GPUS 8
