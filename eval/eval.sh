65c33893d30b0fd44ebb5e57d0636ed5dbff5128
thanhnx12 <thanhnx1209@gmail.com>
2025-09-18 13:51:37 +0000

    eval on 1 gpu

--- eval/eval.sh
+++ eval/eval.sh
@@ -1,3 +1,3 @@
 export MODELS_ROOT="/nfs/gpu_trainee/final-project/models"
 export MODELBIN_ROOT="/nfs/gpu_trainee/final-project/modelbin"
-srun -N 1 --gres=gpu:8 python evaluation.py -m 20b
+srun -N 1 --gres=gpu:1 python evaluation.py -m 20b
